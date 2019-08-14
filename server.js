520f9e0b4091f60012e2757d7ae247d945d9c37d
Stephen Parker <stephenparkhum@outlook.com>
2019-08-14 14:05:03 -0700

    Updating funcs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_Token;
+    const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
